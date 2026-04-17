85ddd757910f75537e1078a0705d631e0469bbb4
HanielUlises <hanielulises2003@gmail.com>
2026-04-16 22:50:14 -0600

    Decoder

--- decoder-ring/src/lib.rs
+++ decoder-ring/src/lib.rs
@@ -26,5 +26,18 @@ fn stats_analysis(text: &str) -> Vec<(char, u32, f32, Option<f32>, f32)> {
 
     let total: u32 = counts.values().sum();
     let eng_freq_map = gen_counts();
-    let freq_eng_map: HashMap<char, f32> = eng_freq_map.iter().map(|k, v| (*k, *v))
+    let freq_eng_map: HashMap<char, f32> =     eng_freq_map.iter().map(|(k, v)| (*k, *v)).collect();
+
+    let mut results = Vec::new();
+
+    for(letter, count) in &counts {
+        let freq = (*count as f32 / total as f32) * 100.0;
+        let eng_freq = eng_freq_map.get(&letter.to_ascii_lowercase()).clone();
+
+        let eng_freq_diff = eng_freq.map_or(0.0, |f|(freq - f).abs());
+
+        results.push((*letter, *count, freq, eng_freq, eng_freq_diff));
+    }  
+
+    results
 } 
