2c11d97a755ca8fb98193139f211bdf57b27d5a4
HanielUlises <hanielulises2003@gmail.com>
2026-09-21 23:33:46 -0600

    ZKP protocol

--- ZKP-Chaum-Pedersen/src/lib.rs
+++ ZKP-Chaum-Pedersen/src/lib.rs
@@ -1,4 +1,15 @@
+use num_bigint::BigUint;
 
-pub fn exponentiate() {
+/// alpha^x mod p 
+/// output = n ^ exp mod p 
+pub fn exponentiate(n: &BigUint, exponent: &BigUint, modulus: &BigUint) -> BigUint {
+    n.modpow(exponent, modulus)
 
+}
+
+/// Output = s = k - c * x mod p
+pub fn solve(k: &BigUint, c: &BigUint, x: &BigUint) -> BigUint {
+    if *k >= c * x {
+        k - c * x
+    }
 }
